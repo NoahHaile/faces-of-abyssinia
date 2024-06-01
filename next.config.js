58eb720910e3a8e1e8bdb33e40a8146acaa23ca6
Noah Hailegiorgis <noahhaile456@gmail.com>
2024-06-01 19:45:35 +0300

    config modified

--- next.config.js
+++ next.config.js
@@ -2,7 +2,16 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  
+ 
+  assetPrefix: "/communityprojects/facesofabyssinia/",
+  async rewrites() {
+    return [
+      {
+        source: `/communityprojects/facesofabyssinia/_next/:path*`,
+        destination: '/_next/:path*'
+      }
+    ]
+  }
   };
   
   module.exports = nextConfig;
